95f574a1b7dde7e8b6920cf6288cf6c5b2a10c6c
tytseng <thomasytseng@gmail.com>
2016-01-30 15:15:25 -0800

    post function working

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var htmlValue = "<!DOCTYPE html>\n<html lang=\"en\">\n\t<head>\n\t</head>"
-  var myCodeMirror = CodeMirror(document.body, {
-    //value: "function myScript(){return 100;}\n",
-    value: htmlValue,
-    mode:  "html",
-    readOnly: true,
-    lineNumbers: true
-  });
+var myFirebaseRef = new Firebase("https://winfoapp.firebaseio.com/");
 
-  //var editor = CodeMirror.fromTextArea(document.getElementById("editorTextArea"), {
-  //  lineNumbers: true,
-  //  mode: "text/htmlmixed",
-  //  matchBrackets: true
-  //});
+document.addEventListener("DOMContentLoaded", function() {
+	var div = document.getElementById('fill');
+	div.innerHTML = "hello";
+	addFile("greatCode", "html");
+	console.log(getFiles());
+
 });
+
+function getFiles(){
+	myFirebaseRef.on("value", function(snapshot) {
+	  return snapshot.val();
+	});
+}
+
+function addFile(fileName, lang){
+	myFirebaseRef.child(fileName).set({
+		type: 'file',
+		language: lang,
+		content: ''
+	});
+}
+
+function loadFiles(){
+	// var i=0;
+ //    var grid = document.createElement('table');
+ //    grid.className = 'grid';
+ //    for (var r=0;r<rows;++r){
+ //        var tr = grid.appendChild(document.createElement('tr'));
+ //        for (var c=0;c<cols;++c){
+ //            var cell = tr.appendChild(document.createElement('td'));
+ //            cell.innerHTML = ++i;
+ //        }
+ //    }
+ //    return grid;
+}
 
