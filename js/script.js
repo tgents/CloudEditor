e8468a4fbd999259606e28f54c2b9e9bf6acb3d1
tytseng <thomasytseng@gmail.com>
2016-01-30 17:56:01 -0800

    mostly functional

--- js/script.js
+++ js/script.js
@@ -3,48 +3,45 @@
 var myFirebaseRef = new Firebase("https://winfoapp.firebaseio.com/");
 
 document.addEventListener("DOMContentLoaded", function() {
+
 	var div = document.getElementById('fill');
 	div.innerHTML = "hello";
-	addFile("greatCode", "html");
-	addFile("greatCode1", "html");
-	addFile("greatCode2", "html");
-	addFile("greatCode3", "html");
-	addFile("greatCode4", "html");
-	//console.log(getFiles());
-
-	//div.innerHTML = "hello";
- 	//addFile("greatCode", "html");
+	// addFile("greatCode", "html");
+	// addFile("greatCode1", "html");
+	// addFile("greatCode2", "html");
+	// addFile("greatCode3", "html");
+	// addFile("greatCode4", "html");
  	getFiles();
 
-  var value = "Type your code here";
-  //showEditor(textEditor, value);
-  //requirejs(["helper/util"], function(util) {
-  //  //This function is called when scripts/helper/util.js is loaded.
-  //  //If util.js calls define(), then this function is not fired until
-  //  //util's dependencies have loaded, and the util argument will hold
-  //  //the module value for "helper/util".
-  //});
-
-  //requirejs([
-  //  "codemirror/lib/codemirror", "codemirror/mode/htmlmixed/htmlmixed"
-  //], function(CodeMirror) {
-  //  CodeMirror.fromTextArea(textEditor, {
-  //    lineNumbers: true,
-  //    mode: "htmlmixed",
-  //    value: value
-  //  });
-  //});
-  //showEditor(div, value);
 });
 
 function showEditor(name, lang, value) {
-	console.log("boo");
-  CodeMirror(document.getElementById('fill'), {
-    value: value,
-    mode:  "text/html",
-    readOnly: false,
-    lineNumbers: true
-  });
+	console.log(name);
+	var div = document.getElementById('fill')
+	div.innerHTML = "";
+	var codem = CodeMirror(div, {
+		value: value,
+		mode:  "text/html",
+		readOnly: false,
+		lineNumbers: true
+	});
+
+	var save = document.createElement('button');
+	var back = document.createElement('button');
+	save.innerHTML = "save";
+	back.innerHTML = "back";
+
+	save.onclick = function(){
+		updateFile(name, codem.getValue());
+	};
+
+	back.onclick = function(){
+		getFiles();
+	};
+
+	div.appendChild(save);
+	div.appendChild(back);
+
 }
 
 
@@ -54,7 +51,7 @@ function getFiles(){
 	});
 }
 
-function updateFile(code){
+function updateFile(fileName, code){
 	myFirebaseRef.child(fileName).update({
 		content: code
 	});
@@ -69,18 +66,25 @@ function addFile(fileName, lang){
 }
 
 function loadFiles(files){
-	console.log(typeof files);
 	var list = document.createElement('ul');
 	list.className = 'files';
 	var keys = Object.keys(files);
-	console.log(files[keys[0]]);
+
+	var type;
+	var val;
+	var key;
 	for (var i = 0; i < keys.length; i++){
-		console.log();
-		var text = keys[i] +"."+ files[keys[i]].language;
+		type = files[keys[i]].language;
+		val = files[keys[i]].content;
+		key = keys[i];
+
+		var text = key +"."+ type;
 		var item = document.createElement('li');
 		item.appendChild(document.createTextNode(text));
 		item.style.padding = "10px";
-		item.addEventListener("click", (showEditor(keys[i], files[keys[i]].language, files[keys[i]].content)), false);
+		item.onclick = function(){
+				showEditor(key, type, val);
+			};
 		list.appendChild(item);
 	}
 	document.getElementById('fill').innerHTML = "";
